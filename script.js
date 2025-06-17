3b80d411f9146890f7c60b7ff9c802a108ee5b1a
bheem-works <shokokomichan098@gmail.com>
2025-06-17 13:09:26 +0545

    AligningProblems

--- script.js
+++ script.js
@@ -5,7 +5,6 @@
 const canvas = document.getElementById("sketch");
 const ctx = canvas.getContext("2d");
 // Setting the background color of the canvas. 
-// Taking the full widht and the heights; 
 var window_width= window.innerWidth;
 var window_height = window.innerHeight;
 canvas.width= window_width;
@@ -13,11 +12,20 @@ canvas.height = window_height;
 //Set the background color 
 ctx.fillStyle = "pink";
 ctx.fillRect(0,0,window_width,window_height);
+
+//Rectangle; 
+ctx.beginPath();
+ctx.strokeStyle = "white";
+ctx.lineWidth = 15;
+ctx.fillStyle = "black";
+ctx.rect(200,80,1000,900);
+ctx.fill();
+
 // Stroke
 ctx.lineWidth= 8;
-ctx.strokeStyle = "orange";
-ctx.moveTo(30,140); // First param is used to move left , right and another is used to move to the up and down; 
-ctx.lineTo(140,140); // Firs param is used to increase the line size and another is used and second param is used to set the directions;
+ctx.strokeStyle = "white";
+ctx.moveTo(100,140); // First param is used to move left , right and another is used to move to the up and down; 
+ctx.lineTo(200,140); // Firs param is used to increase the line size and another is used and second param is used to set the directions;
 ctx.stroke(); // To call the stroke. It is a methods; 
 
 // Meaning of the begin paths; 
@@ -28,5 +36,5 @@ ctx.stroke(); // To call the stroke. It is a methods;
 ctx.beginPath(); // New Path;
 ctx.strokeStyle = "red";
 ctx.lineWidth = 30;
-ctx.arc(100,75,15,0,Math.PI * 2,false);
+ctx.arc(500,750,15,0,Math.PI * 2,false);
 ctx.stroke();
