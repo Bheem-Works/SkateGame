14300e8e63352934833311a5beec618e634dc3e6
bheem-works <shokokomichan098@gmail.com>
2025-06-17 16:45:49 +0545

    CirlceCompleted

--- script.js
+++ script.js
@@ -1,40 +1,36 @@
 // Note - Canvas is used for draw the elements by using javascript; And canvas use the 2d graphis animation. 
 // getContext helps to get the context element for the drawing; 
 // 2d let use draw the shape and size. inside the browser.
-
 const canvas = document.getElementById("sketch");
-const ctx = canvas.getContext("2d");
-// Setting the background color of the canvas. 
-var window_width= window.innerWidth;
-var window_height = window.innerHeight;
-canvas.width= window_width;
-canvas.height = window_height;
-//Set the background color 
-ctx.fillStyle = "pink";
-ctx.fillRect(0,0,window_width,window_height);
+    const ctx = canvas.getContext("2d");
 
-//Rectangle; 
-ctx.beginPath();
-ctx.strokeStyle = "white";
-ctx.lineWidth = 15;
-ctx.fillStyle = "black";
-ctx.rect(200,80,1000,900);
-ctx.fill();
+    // Set canvas size to window dimensions
+    const window_width = window.innerWidth;
+    const window_height = window.innerHeight;
+    canvas.width = window_width;
+    canvas.height = window_height;
 
-// Stroke
-ctx.lineWidth= 8;
-ctx.strokeStyle = "white";
-ctx.moveTo(100,140); // First param is used to move left , right and another is used to move to the up and down; 
-ctx.lineTo(200,140); // Firs param is used to increase the line size and another is used and second param is used to set the directions;
-ctx.stroke(); // To call the stroke. It is a methods; 
+    // Set background color
+    ctx.fillStyle = "pink";
+    ctx.fillRect(0, 0, window_width, window_height);
 
-// Meaning of the begin paths; 
-// Defination > It is used to create a new path and reset the current path and starts the new empty paths in the canvas 2d rendering context; 
-// without the beginPath() the canvas return the previous and new drawing command like lineTo, moveTo will added to it. creating a unwanted shapes. 
+    // Rectangle
+    ctx.beginPath();
+    ctx.fillStyle = "black";
+    ctx.fillRect(200, 80, 1000, 900);
+    ctx.fill();
 
-// Arc(circle);
-ctx.beginPath(); // New Path;
-ctx.strokeStyle = "red";
-ctx.lineWidth = 30;
-ctx.arc(500,750,15,0,Math.PI * 2,false);
-ctx.stroke();
+    // Line
+    ctx.beginPath();
+    ctx.strokeStyle = "white";
+    ctx.lineWidth = 15;
+    ctx.moveTo(450, 765); // Starting point
+    ctx.lineTo(550, 765); // Endpoint, moving down 100 pixels
+    ctx.stroke();
+
+    // Circle
+    ctx.beginPath();
+    ctx.strokeStyle = "red";
+    ctx.lineWidth = 30;
+    ctx.arc(500, 720, 15, 0, Math.PI * 2, false);
+    ctx.stroke();
